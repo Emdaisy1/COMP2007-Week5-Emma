e0af1f42b2e3d85b25f72ac24fb15c80c03a30fa
Emdaisy1 <hilborn_emmav@live.ca>
2016-06-13 17:44:00 -0400

    Text fix

--- COMP2007-Week5/Scripts/app.js
+++ COMP2007-Week5/Scripts/app.js
@@ -2,6 +2,6 @@
 
 $(document).ready(function () {
     $("a.btn.btn-danger.btn-sm").click(function () {
-        return confirm("Are you sure you want to delete this person?");
+        return confirm("Are you sure you want to delete this record?");
     })
 })
